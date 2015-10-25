eb9fb4b68e2049597fb6fb4028bc18182b616013
Brian Tan <briantanw@gmail.com>
2015-10-25 00:23:55 -0700

    Mexican trying to jump from Donald Trump's wall

---
+++ FlappyGame/Assets/CharacterController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public Vector2 jumpForce = new Vector2(0, 300);
+
+	// Use this for initialization
+	void Start ()
+    {
+        // Grab the rigidbody reference.
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Reset all velocity so there is no force going down
+            rb.velocity = Vector2.zero;
+            // add jump force
+            rb.AddForce(jumpForce);
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Die();
+    }
+
+    void Die()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
---
+++ FlappyGame/Assets/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject obstacle;
+    public float repeatRate = 3f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        InvokeRepeating("CreateObstacle", 1f, repeatRate);
+	}
+	
+    void CreateObstacle()
+    {
+        Instantiate(obstacle);
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+
+}
